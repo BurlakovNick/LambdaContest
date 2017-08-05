ffbe870be6defabc872b09e00b3a7fa7b212d248
Burlakov Nick <burlakov.nick@gmail.com>
2017-08-05 20:04:35 +0500

    Добавил зерг раш

--- Lambda/run.bat
+++ Lambda/run.bat
@@ -1,6 +1,6 @@
-start Server\bin\Debug\Server.exe oxford 2
+start Server\bin\Debug\Server.exe lambda 2
 #start Client\bin\Debug\Client.exe greedy
 #start Client\bin\Debug\Client.exe stupidgreedy
-#start Client\bin\Debug\Client.exe greedy
-start Client\bin\Debug\Client.exe stupidgreedy
-#start Client\bin\Debug\Client.exe greedy
+#start Client\bin\Debug\Client.exe greedydesired
+start Client\bin\Debug\Client.exe GreedyEdgeChooserPunter
+start Client\bin\Debug\Client.exe GreedyEdgeChooserPunterWithZergRush
