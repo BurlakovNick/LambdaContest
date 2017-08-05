19d1831f8592dd4af0408a1eb6725c01f4607b03
Burlakov Nick <burlakov.nick@gmail.com>
2017-08-05 11:09:17 +0500

    OfflinePlayer project + common serializer

---
+++ Lambda/Core/Infrastructure/Serializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Core.Infrastructure
+{
+    public class Serializer
+    {
+        public T Deserialize<T>(string message)
+        {
+            var delimeter = message.IndexOf(":", StringComparison.Ordinal);
+            if (delimeter == -1)
+            {
+                throw new Exception($"No delimeter in message: {message}");
+            }
+
+            var size = int.Parse(message.Substring(0, delimeter));
+            var body = message.Substring(delimeter + 1);
+            return JsonConvert.DeserializeObject<T>(body);
+        }
+
+        public string Serialize<T>(T message)
+        {
+            var body = JsonConvert.SerializeObject(message);
+            return $"{body.Length}:{body}";
+        }
+    }
+}
