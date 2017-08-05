b4aa03c0a72cfb82156ed0a8511733a84190a26d
mif <mif.open@gmail.com>
2017-08-05 18:58:33 +0500

    все карты + цвета нормальные

--- Lambda/WebApp/Scripts/viewer.js
+++ Lambda/WebApp/Scripts/viewer.js
@@ -49,26 +49,10 @@ function updateEdgeOwner(punter, source, target) {
 
 const colours =
 [
-    "#1f77b4",
-    "#aec7e8",
-    "#ff7f0e",
-    "#ffbb78",
-    "#2ca02c",
-    "#98df8a",
-    "#d62728",
-    "#ff9896",
-    "#9467bd",
-    "#c5b0d5",
-    "#8c564b",
-    "#c49c94",
-    "#e377c2",
-    "#f7b6d2",
-    "#7f7f7f",
-    "#c7c7c7",
-    "#bcbd22",
-    "#dbdb8d",
-    "#17becf",
-    "#9edae5"
+    "#FF0000",
+    "#00FF00",
+    "#f5f904",
+    "#17becf"
 ];
 
 function getPunterColour(punter) {
@@ -158,12 +142,14 @@ function selectMap(url) {
 }
 
 function bindCoreHandlers() {
-    cy.edges().on("mouseover", function (evt) {
-        this.style("content", this.data("owner"));
-    });
-    cy.edges().on("mouseout", function (evt) {
-        this.style("content", "");
-    });
+    cy.edges().on("mouseover",
+        function(evt) {
+            this.style("content", this.data("owner"));
+        });
+    cy.edges().on("mouseout",
+        function(evt) {
+            this.style("content", "");
+        });
 }
 
 $(function() {
