3f8eac2e2dfa13ced1979e93100865f7b317d4fa
oksanchike <oksanchike@skbkontur.ru>
2017-08-06 15:21:55 +0500

    переписали GreedyDesiredPunter на новые рельсы

--- Lambda/Core/Components/Component.cs
+++ Lambda/Core/Components/Component.cs
@@ -1,46 +1,33 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Core.Objects;
 
 namespace Core.Components
 {
+
     public class Component : IComparable
     {
         public List<Node> Nodes { get; set; }
-        public List<Node> Mines { get; set; }
-        public Dictionary<int, long> Scores { get; set; } = new Dictionary<int, long>();
-        public long SelfScore { get; set; }
+        public ComponentScore Score { get; set; }
         public int SubtreeSize { get; set; }
 
         public Component(Node node, Node[] mines, IScorer scorer)
         {
-            Nodes = new List<Node> {node};
-            Mines = Nodes.Where(x => x.IsMine).ToList();
-            foreach (var mine in mines)
-            {
-                var d = scorer.GetDistance(mine, node);
-                Scores.Add(mine.Id, d * d);
-            }
-            SelfScore = Mines.Sum(x => Scores[x.Id]);
+            Nodes = new List<Node> { node };
+            Score = new ComponentScore(node, mines, scorer);
         }
 
         public void Union(Component other)
         {
             Nodes.AddRange(other.Nodes);
-            Mines.AddRange(other.Mines);
-            foreach (var score in other.Scores)
-            {
-                Scores[score.Key] += score.Value;
-            }
-            SelfScore = Mines.Sum(x => Scores[x.Id]);
+            Score.Add(other.Score);
         }
 
         public int CompareTo(object obj)
         {
-            if (SubtreeSize != ((Component) obj).SubtreeSize)
+            if (SubtreeSize != ((Component)obj).SubtreeSize)
             {
-                return ((Component) obj).SubtreeSize - SubtreeSize;
+                return ((Component)obj).SubtreeSize - SubtreeSize;
             }
             return GetHashCode() - obj.GetHashCode();
         }
--- Lambda/Core/Components/ComponentManager.cs
+++ Lambda/Core/Components/ComponentManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Core.Objects;
@@ -36,6 +37,7 @@ namespace Core.Components
 
         public void ClaimEdge(Node a, Node b)
         {
+            //todo: придумать что делать с desireComponent при слиянии
             var sourceComponent = nodeToComponent[a.Id];
             var targetComponent = nodeToComponent[b.Id];
             if (sourceComponent != targetComponent)
@@ -51,12 +53,181 @@ namespace Core.Components
 
         public int FindShortestPathLength(Component a, Component b)
         {
-            throw new System.NotImplementedException();
+            var distance = Bfs(a, b);
+            return distance.ContainsKey(b) ? distance[b] : int.MaxValue;
         }
 
         public DesireComponent FindShortestPath(Component a, Component b)
         {
-            throw new System.NotImplementedException();
+            var distanceFromA = Bfs(a, b);
+            var distanceFromB = Bfs(b, a);
+
+            var length = distanceFromA[b];
+
+            var nodes = distanceFromA
+                .Where(x => distanceFromB.ContainsKey(x.Key) && x.Value + distanceFromB[x.Key] == length)
+                .OrderBy(x => x.Value)
+                .Select(x => x.Key)
+                .ToArray();
+
+            var dp = new Dictionary<Component, ComponentScore>();
+            var parent = new Dictionary<Component, Component>();
+
+            dp[a] = a.Score;
+            foreach (var node in nodes.Skip(1))
+            {
+                var prevs = GetEdges(node)
+                    .Select(x => x.Item1)
+                    .Where(x => dp.ContainsKey(x) && distanceFromA[x] == distanceFromA[node] + 1);
+
+                foreach (var prev in prevs)
+                {
+                    var score = dp[prev].Clone().Add(node.Score).Add(b.Score);
+                    if (!dp.ContainsKey(node) || dp[node].SelfScore < score.SelfScore)
+                    {
+                        dp[node] = score;
+                        parent[node] = prev;
+                    }
+                }
+            }
+
+            var result = new DesireComponent();
+            result.Components.Add(a);
+            while (a != b)
+            {
+                result.Components.Add(b);
+                b = parent[b];
+            }
+
+            return result;
+        }
+
+        public DesireComponent FindGreedyFullComponent(int size)
+        {
+            var result = new DesireComponent();
+
+            result.Root = GetBestComponentByChart(size / 2);
+            result.Components.Add(result.Root);
+
+            var desireScore = result.Root.Score.Clone();
+
+            var S = new SortedSet<(long, Component)>();
+
+            foreach (var c in GetEdges(result.Root).Select(x => x.Item1))
+            {
+                S.Add((desireScore.Clone().Add(c.Score).SelfScore, c));
+            }
+
+            for (int i = 0; i < size - 1; ++i)
+            {
+                while (S.Count != 0 && result.Components.Contains(S.Max.Item2))
+                {
+                    S.Remove(S.Max);
+                }
+                if (S.Count == 0)
+                {
+                    break;
+                }
+                var newComponent = S.Max.Item2;
+                result.Components.Add(newComponent);
+                desireScore.Add(newComponent.Score);
+                S.Remove(S.Max);
+
+                if (newComponent.Score.Mines.Any())
+                {
+                    foreach (var c in components.Where(c => !result.Components.Contains(c)))
+                    {
+                        S.Add((desireScore.Clone().Add(c.Score).SelfScore - desireScore.SelfScore, c));
+                    }
+                }
+                else
+                {
+                    foreach (var c in newComponent.Nodes.SelectMany(n => map.GetAvaliableEdges(n.Id, punter).Select(e => nodeToComponent[e.Item1.Id])).Distinct().Where(x => !result.Components.Contains(x)))
+                    {
+                        S.Add((desireScore.Clone().Add(c.Score).SelfScore - desireScore.SelfScore, c));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Component GetBestComponentByChart(int chartSize)
+        {
+            var mineToScore = mines.ToDictionary(m => m.Id, m => components.Select(n => n.Score.Scores[m.Id]).OrderByDescending(s => s).Take(chartSize).Sum());
+            return components.OrderByDescending(n => n.Score.Mines.Sum(x => mineToScore[x.Id])).First();
+        }
+
+        private Dictionary<Component, int> Bfs(Component a, Component b)
+        {
+            var queue = new Queue<Component>();
+            queue.Enqueue(a);
+            var distance = new Dictionary<Component, int>();
+            distance[a] = 0;
+
+            while (queue.Any())
+            {
+                var current = queue.Dequeue();
+                var dist = distance[current];
+                foreach (var neighbour in GetEdges(current))
+                {
+                    if (!distance.ContainsKey(neighbour.Item1))
+                    {
+                        distance[neighbour.Item1] = dist + 1;
+                        queue.Enqueue(neighbour.Item1);
+                    }
+                    if (neighbour.Item1 == b)
+                    {
+                        return distance;
+                    }
+                }
+            }
+            return distance;
+        }
+
+        public Edge GetMineEdge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Edge GetMostExpensiveEdge()
+        {
+            return map.Edges
+                .Where(x => x.Punter == null)
+                .GroupBy(x => (nodeToComponent[x.Source.Id], nodeToComponent[x.Target.Id]))
+                .OrderByDescending(x => x.Key.Item1.Score.Clone().Add(x.Key.Item2.Score).SelfScore
+                                        - x.Key.Item1.Score.SelfScore - x.Key.Item2.Score.SelfScore)
+                .First()
+                .First();
+        }
+
+        public Edge GetMostExpensiveFromComponentEdge(Component from)
+        {
+            var neighbours = GetEdges(from).Select(x => x.Item1).ToArray();
+
+            Component best = null;
+            long maxScore = -1;
+            foreach (var neighbour in neighbours)
+            {
+                var score = from.Score.Clone().Add(neighbour.Score).SelfScore;
+                if (score > maxScore)
+                {
+                    maxScore = score;
+                    best = neighbour;
+                }
+            }
+
+            return best == null ? GetMostExpensiveEdge() : GetFreeEdge(from, best);
+        }
+
+        public Edge GetMaxSubtreeEdge(DesireComponent desire)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Edge GetFragileEdge(DesireComponent desire)
+        {
+            throw new NotImplementedException();
         }
 
         private (Component, int)[] GetEdges(Component from)
@@ -64,8 +235,16 @@ namespace Core.Components
             return from.Nodes.SelectMany(n => map.GetFreeEdges(n.Id))
                 .GroupBy(n => nodeToComponent[n.Item1.Id])
                 .Select(x => (x.Key, x.Count()))
+                .Where(x => x.Item1 != from)
                 .ToArray();
         }
 
+        private Edge GetFreeEdge(Component a, Component b)
+        {
+            return a.Nodes
+                .SelectMany(n => map.GetFreeEdges(n.Id))
+                .First(n => nodeToComponent[n.Item1.Id] == b)
+                .Item2;
+        }
     }
 }
