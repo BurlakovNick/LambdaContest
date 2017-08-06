a33aa58413f2be9863b72c369e27bd40c4e7373f
Burlakov Nick <burlakov.nick@gmail.com>
2017-08-06 10:36:15 +0500

    fixes

--- Lambda/run.bat
+++ Lambda/run.bat
@@ -1,9 +1,8 @@
 start Server\bin\Debug\Server.exe oxford 6
-#start Client\bin\Debug\Client.exe greedy
-#start Client\bin\Debug\Client.exe stupidgreedy
-start Client\bin\Debug\Client.exe stupidgreedy
-start Client\bin\Debug\Client.exe greedydesired
-start Client\bin\Debug\Client.exe greedy
-start Client\bin\Debug\Client.exe stupidgreedy
-start Client\bin\Debug\Client.exe greedydesired
-start Client\bin\Debug\Client.exe greedy
+start Client\bin\Debug\Client.exe GreedyComponentPunter
+start Client\bin\Debug\Client.exe AlwaysFirstPunter
+start Client\bin\Debug\Client.exe GreedyDesiredPunter
+start Client\bin\Debug\Client.exe GreedyEdgeChooserPunter
+start Client\bin\Debug\Client.exe GreedyEdgeChooserPunterWithZergRush
+start Client\bin\Debug\Client.exe GreedyEdgeChooserPunterWithStupidZergRush
+
