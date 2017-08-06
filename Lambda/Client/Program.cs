c0d5a2c82d6cedbefd49f54bc9fce0dc777d21e1
mif <mif.open@gmail.com>
2017-08-06 21:48:19 +0500

    какие-то фиксы

--- Lambda/Client/Program.cs
+++ Lambda/Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using Core;
 
 namespace Client
@@ -7,14 +9,29 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            var punterName = args[0];
-            var server = args.Length > 1 ? args[1] : "localhost";
-            var port = args.Length > 2 ? args[2] : "7777";
+            try
+            {
+                Log("Client started " + args[0]);
+                var punterName = args[0];
+                var server = args.Length > 1 ? args[1] : "localhost";
+                var port = args.Length > 2 ? args[2] : "7777";
 
-            var punter = PunterFactory.Create(punterName);
-            var client = new OnlineClient(punter);
-            client.Start(server, port);
-            Console.ReadLine();
+                var punter = PunterFactory.Create(punterName);
+                var client = new OnlineClient(punter);
+                client.Start(server, port);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Log("Error:" + e);
+                throw;
+            }
+        }
+
+        private static void Log(string text)
+        {
+            Console.WriteLine(text);
+            File.WriteAllText($"client{Process.GetCurrentProcess().Id}Log.txt", text);
         }
     }
 }
